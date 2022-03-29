79ba795eed79d6e39029d15d89b258417ae65382
anywhere27 <ada_hack217@hackademy.mx>
2022-03-28 23:23:45 -0600

    suma de numero enteros

---
+++ sumaNumEnteros.py
@@ -0,0 +1,10 @@
+def suma(x, y):
+   return valor1 + valor2
+
+
+valor1 = int(input("ingrese el primero valor: "))
+valor2 = int(input("ingrese el segundo valor: "))
+
+resultado = suma(valor1, valor2)
+print("el resultado es %d" % (resultado))
+
